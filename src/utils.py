98655f87e99c110004fbc210102d71218e4782c8
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-09-23 17:53:52 -0700

    feat: add class for calculating and keeping track of the next 12 first saturdays

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from dateutil.rrule import MONTHLY, SA, rrule
+from dateutil.utils import today
+
+
+class DateCalculator:
+    """For keeping track of the first saturday of every month.
+
+    Calculates the next 12 first Saturdays and then keeps track of the next target Saturday.
+    """
+
+    _next_saturdays: list[datetime]
+    target_saturday: datetime
+
+    def __init__(self) -> None:
+        self._next_saturdays = self._generate_saturdays()
+        self.target_saturday = self._select_next_target_saturday()
+
+    def _generate_saturdays(self) -> list[datetime]:
+        return list(rrule(MONTHLY, byweekday=SA(1), dtstart=today(), count=12))
+
+    def _select_next_target_saturday(self) -> datetime:
+        if not self._next_saturdays:
+            self._next_saturdays = self._generate_saturdays()
+
+        return self._next_saturdays.pop(0)
+
+
+if __name__ == "__main__":
+    date_calc = DateCalculator()
+    print(date_calc._next_saturdays)
+    print(date_calc.target_saturday)
