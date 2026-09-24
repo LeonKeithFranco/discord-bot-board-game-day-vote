d8d50459fef5961fef3f1a28658691faee3ed40e
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-09-23 20:27:16 -0700

    feat: add method to include certain dates based off of statutory holidays

--- src/date.py
+++ src/date.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime, time
 from zoneinfo import ZoneInfo
 
+import holidays
 from dateutil.relativedelta import relativedelta
 from dateutil.rrule import MONTHLY, SA, WEEKLY, rrule
 
@@ -45,8 +46,32 @@ def get_all_saturdays_in_month(d: date) -> list[date]:
     ]
 
 
+def get_valid_statutory_holidays_in_month(d: date) -> list[date]:
+    """Returns the dates that are valid relative to staturoy holiday dates.
+
+    If holiday dates land a Friday, it is returned. If it lands on a Monday, the Sunday
+    previous is returned instead. Any other days are discarded.
+    """
+    bc_holidays = holidays.Canada(subdiv="BC", years=_TODAY.year)
+
+    valid_holidays_this_month: list[date] = []
+
+    for day in bc_holidays:
+        if day.month != d.month:
+            continue
+
+        match day.isoweekday():
+            case 1:
+                valid_holidays_this_month.append(day - relativedelta(days=1))
+            case 5:
+                valid_holidays_this_month.append(day)
+
+    return valid_holidays_this_month
+
+
 if __name__ == "__main__":
     date_calc = DateCalculator()
     print(date_calc._next_saturdays)
     print(date_calc.target_saturday)
     print(get_all_saturdays_in_month(_TODAY.date()))
+    print(get_valid_statutory_holidays_in_month(_TODAY.date()))
